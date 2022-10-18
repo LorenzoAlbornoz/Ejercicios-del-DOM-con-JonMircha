ff90ec874fb0bf364c0c067d3172f747c0bcc830
LorenzoAlbornoz <lorenzoa.28.la@gmail.com>
2022-10-18 12:29:39 -0300

    Dark Theme

--- js/index_dom.js
+++ js/index_dom.js
@@ -3,6 +3,7 @@ import { digitalClock, alarm } from "./dom/reloj.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
 import countdown from "./dom/cuenta_regresiva.js";
 import scrollTopButton from "./dom/boton_Scroll.js";
+import darkTheme from "./dom/tema_oscuro.js";
 
 const d = document;
 
@@ -12,6 +13,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     alarm("assets/mario-bros.mp3", "#activar-alarma", "#desactivar-alarma")
     countdown("countdown", "October 17, 2023, 11:10:00", "Feliz cumpleaños!!! 😊")
     scrollTopButton(".scroll-top-btn")
+    darkTheme(".dark-theme-btn", "dark-mode")
 })
 
 d.addEventListener("keydown", (e) => {
