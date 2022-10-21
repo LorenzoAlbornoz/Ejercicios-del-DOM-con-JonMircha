165ef40b2f0c87c8cefff9d4edcb80cd1c98d33f
LorenzoAlbornoz <lorenzoa.28.la@gmail.com>
2022-10-21 10:00:04 -0300

    Deteccion WebCam y Geolocalizacion

--- js/index_dom.js
+++ js/index_dom.js
@@ -8,6 +8,8 @@ import responsiveMedia from "./dom/objeto_responsive.js";
 import responsiveTester from "./dom/prueba_responsive.js";
 import userDeviceInfo from "./dom/deteccion_dispositivos.js";
 import networkStatus from "./dom/deteccion_red.js";
+import webCam from "./dom/detenccion_webcam.js";
+import getGeolocation from "./dom/geolocalizacion.js";
 
 const d = document;
 
@@ -21,7 +23,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
     responsiveMedia("gmaps","(min-width:1024px)", `<a href="https://goo.gl/maps/gzZMWT8FRcvjSeSe9" target="_black" rel="noopener">Ver Mapa</a>`, `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.661630623397!2d-99.16759935!3d19.4270206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff35f5bd1563%3A0x6c366f0e2de02ff7!2sEl%20%C3%81ngel%20de%20la%20Independencia!5e0!3m2!1ses-419!2sar!4v1666187118954!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`)
     responsiveTester("responsive-tester")
     userDeviceInfo("user-device")
-    
+    webCam("webcam")
+    getGeolocation("geolocation")
 })
 
 d.addEventListener("keydown", (e) => {
